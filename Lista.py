9333b4a2cfbd87a84d82eaa993fcc3e33a44ada3
esteban027 <dbermudezb@unbosque.edu.co>
2015-11-15 00:10:09 -0500

    v 1.8

--- Lista.py
+++ Lista.py
@@ -2,9 +2,16 @@ class Lista:
     
     def __init__(self):
         self.items=[]
-    def agregarAlFinal():
+    def agregarAlFinal(self,x):
         self.items.append(x)
-    def agregarAlInicio(self):
+    def agregarAlInicio(self,x):
+        self.items.insert(0, x)
+    def eliminarAlinicio(self):
+        self.items.pop(0)
+    def eliminarAlFinal(self):
+        self.items.pop()
+    
+        
         
         
     
